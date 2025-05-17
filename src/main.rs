93248b8cfa2c7c3b0c69a5c5abbbdb1947ee140b
carlolalu <carlolanziluciani@gmail.com>
2025-05-17 18:16:55 +0200

    Basic CLI using 'clap' and 'anyhow'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result, anyhow};
+use clap::Parser;
+use std::path::PathBuf;
+
+/// A program to execute `pdfunite` on a folder with maximum 3 levels and obtain a PDF-file with a ToC of all the tree.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Folder containing the pdfs
+    #[arg(short, long)]
+    root_pdfs: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let entries: Vec<_> = std::fs::read_dir(&args.root_pdfs)
+        .with_context(|| format!("Could not read file `{}`", args.root_pdfs.display()))?
+        .collect();
+
+    println!("Your entries are: {:?}", entries);
+
+    Ok(())
 }
