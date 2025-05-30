ca8fc6b5493e7fe2264470194e2decfc731e4304
carlolalu <carlolanziluciani@gmail.com>
2025-05-30 13:47:23 +0200

    Add pdfium

--- src/main.rs
+++ src/main.rs
@@ -21,5 +21,13 @@ fn main() -> Result<()> {
 
     println!("Your entries are: {:?}", entries);
 
+    use pdfium_render::prelude::*;
+
+    let pdfium = Pdfium::new(
+        Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path("./"))
+            .or_else(|_| Pdfium::bind_to_system_library())
+            .unwrap() // Or use the ? unwrapping operator to pass any error up to the caller
+    );
+
     Ok(())
 }
